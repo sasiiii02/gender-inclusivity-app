3274db5e029b77456f338ea460ad2238505e19d7
sasiiii02 <sasikaru15@gmail.com>
2026-02-18 10:03:36 +0530

    Server setup and db connection

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDb from "./config/db.js";
+
+dotenv.config();
+connectDb();
+
+const app= express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("API is running")
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+    
+})
