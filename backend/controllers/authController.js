cb317df006aaf7a740c24914141386c3e9f76b74
sasiiii02 <sasikaru15@gmail.com>
2026-02-18 10:40:38 +0530

    User login based on roles

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -47,3 +47,42 @@ export const registerUser = async (req,res) => {
         res.status(500).json({message: error.message});
     }
 }
+
+
+// ================= LOGIN =================
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Generate JWT
+    const token = generateToken(user);
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
