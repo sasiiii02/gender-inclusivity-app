c5747fd4c7c25918122bd28124aaea5eb85e4369
dasunthathsara <thathsarahema18@gmail.com>
2026-02-26 22:27:40 +0530

    fix : solve dev merge conflict

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,7 @@ import protect from "./middleware/authMiddleware.js";
 import eCampaignRoutes from "./routes/eCampaignRoutes.js";
 import eEventRoutes from "./routes/eEventRoutes.js";
 import eRegistrationRoutes from "./routes/eRegistrationRoutes.js";
-
-// Quiz routes
-import QMQuizRoutes from "./routes/QM-quizRoutes.js";
-import QMStudentQuizRoutes from "./routes/QM-studentQuizRoutes.js";
-import QMQuestion from "./models/QM-Question.js";
-
-// AI Explanation routes
-import QMAiRoutes from "./routes/QM-aiRoutes.js";
+import eFeedbackRoutes from "./routes/eFeedbackRoutes.js";
 
 dotenv.config();
 connectDb();
@@ -35,14 +28,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/campaigns", eCampaignRoutes);
 app.use("/api/events", eEventRoutes);
 app.use("/api/registrations", eRegistrationRoutes);
-
-// Quiz routes
-app.use("/api/quizzes", QMQuizRoutes);
-app.use("/api/student/quiz", QMStudentQuizRoutes);
-app.use("/api/questions", QMQuestion);
-
-// AI Explanation routes
-app.use("/api/ai", QMAiRoutes);
+app.use("/api/feedbacks", eFeedbackRoutes);
 
 // Protected test route
 app.get("/api/protected", protect, (req, res) => {
